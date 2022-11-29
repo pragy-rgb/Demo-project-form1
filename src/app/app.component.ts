9c26c83f468f46f84530b9c9db4277cc8458c300
pragy-rgb <78215061+pragy-rgb@users.noreply.github.com>
2022-11-29 13:09:47 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  templateUrl: './register/register.component.html',
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule as ad } from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 import { BrowserModule } from '@angular/platform-browser';
@@ -9,10 +9,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { RegisterComponent } from './register/register.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [AppComponent, ad, MatInputModule],
-  imports: [BrowserModule, FormsModule, HttpClientModule, RegisterComponent],
+  declarations: [AppComponent, RegisterComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
