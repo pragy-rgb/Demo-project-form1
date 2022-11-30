fbae95c58f4ffb4ea82734ccfc441b91f68527b2
pragy-rgb <78215061+pragy-rgb@users.noreply.github.com>
2022-11-30 16:13:06 +0530

    2 files modified, 4 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MaterialModule } from './material/material.module';
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
-import { EmployeeComponent } from './employees/employee/employee.component';
-import { EmployeeService } from './shared/employee.service';
 
 @NgModule({
-  declarations: [AppComponent, EmployeesComponent, EmployeeComponent],
-  imports: [
-    BrowserModule,
-    MaterialModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-  ],
-  providers: [EmployeeService],
+  declarations: [AppComponent, EmployeesComponent],
+  imports: [BrowserModule, ReactiveFormsModule, BrowserAnimationsModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
