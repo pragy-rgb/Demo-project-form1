1e0188a3a250e4e0179ce8efca7e0ba1d908ef1f
pragy-rgb <78215061+pragy-rgb@users.noreply.github.com>
2022-11-30 16:17:07 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { EmployeesComponent } from './employees/employees.component';
 
 @NgModule({
   declarations: [AppComponent, EmployeesComponent],
-  imports: [BrowserModule, ReactiveFormsModule, BrowserAnimationsModule],
+  imports: [BrowserModule, ReactiveFormsModule, MatToolbarModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
